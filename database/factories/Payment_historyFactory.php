2a3ec106436e5d2cdbe573b052089b26f6603d89
Andrea Cosio <andrea.cosio@usach.cl>
2022-06-07 00:09:07 -0400

    [Andrea Cosio] - Creación de factories para genero y payment

---
+++ database/factories/Payment_historyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payment_history>
+ */
+class Payment_historyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+
+            'monto'=>$this->faker->numberBetween($min = 0, $max = 10000),
+            'metodo pago'=>$this->faker->creditCardType(),
+            'user_pay'=>$this->faker->numberBetween($min = 0, $max = 100)
+            
+        ];
+    }
+}
