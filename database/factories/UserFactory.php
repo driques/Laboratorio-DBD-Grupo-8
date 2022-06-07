59c2d876703ebf3cd4a36bba6d3f2a5613acd6c1
Alen Galindo Perez <alen.galindo@usach.cl>
2022-06-06 23:15:19 -0400

    [Alen Galindo] - Agregados faker de User y Country

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'password' => $this->faker->password,
+            'plan' =>  $this->faker->boolean,
+            'birth_year' => $this->faker->date($format = 'Y-m-d', $max = 'now')
+            //'id_pais' => $this->faker->numberBetween($min = 1, $max = 193),
+            //'id_rol' => $this->faker->numberBetween($min = 1, $max = 3)
+        ];
+    }
+}
