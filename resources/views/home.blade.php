10621754a3f7e0079ad67c1c51638977b2be6be2
darsmen32@gmail.com <darsmen32@gmail.com>
2022-06-23 10:49:46 -0400

    [Diego Riquelme] - Arreglo cambios menores
    Cambios menores en el carousel.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,94 +2,212 @@
 <html lang="es">
 
 <head>
-    <style>
-        #grid1{
-            /*width: 1200px;*/
-            min-height: 720px;
-            height: auto;
-            /*margin: 80px auto 0px auto;*/
-            background-image: linear-gradient(to bottom, rgb(63, 191, 203), rgb(196,0,84),rgb(31, 33, 37));
-            /*padding: 10px 10px; */
-            overflow:auto
+  <style>
+    #grid1 {
+      /*width: 1200px;*/
+      min-height: 720px;
+      height: auto;
+      /*margin: 80px auto 0px auto;*/
+      background-image: linear-gradient(to bottom,rgb(73,152,245), rgb(138,199,245), rgb(31, 33, 37));
+      /*padding: 10px 10px; */
+      overflow: auto
     }
-    </style>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DEBEDE Music</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link href="resources/home.css" rel="stylesheet" type="text/css">
+
+    * {
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: Verdana, sans-serif;
+    }
+
+    .mySlides {
+      display: none;
+    }
+
+    img {
+      vertical-align: middle;
+    }
+
+    /* Slideshow container */
+    .slideshow-container {
+      max-width: 1000px;
+      position: relative;
+      margin: auto;
+    }
+
+    /* Caption text */
+    .text {
+      color: #f2f2f2;
+      font-size: 15px;
+      padding: 8px 12px;
+      position: absolute;
+      bottom: 8px;
+      width: 100%;
+      text-align: center;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+      color: #f2f2f2;
+      font-size: 12px;
+      padding: 8px 12px;
+      position: absolute;
+      top: 0;
+    }
+
+    /* The dots/bullets/indicators */
+    .dot {
+      height: 15px;
+      width: 15px;
+      margin: 0 2px;
+      background-color: #bbb;
+      border-radius: 50%;
+      display: inline-block;
+      transition: background-color 0.6s ease;
+    }
+
+    .active {
+      background-color: #717171;
+    }
+
+    /* Fading animation */
+    .fade {
+      animation-name: fade;
+      animation-duration: 1.5s;
+    }
+
+    @keyframes fade {
+
+      from {
+        opacity: .4
+      }
+
+      to {
+        opacity: 1
+      }
+    }
+
+    /* On smaller screens, decrease text size */
+    @media only screen and (max-width: 300px) {
+      .text {
+        font-size: 11px
+      }
+    }
+  </style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DEBEDE Music</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link href="resources/home.css" rel="stylesheet" type="text/css">
 </head>
 
-<body style="background-color:rgb(33, 37, 41);">
-    <!-- Cracion de navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="https://i.ibb.co/KKnBn4P/1-removebg-preview.png" width="40" height="40" class="d-inline-block align-mid" alt="">
-            DEBEDE Music
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav right-left">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Quienes somos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Ayuda</a>
-              </li>
-              <div class="d-flex align-items-right">
-                <button type="button" class="btn btn-link pl-auto me-2">
-                  Iniciar sesión
-                </button>
-                <button type="button" class="btn btn-primary pl-auto me-3">
-                    Registrarse
-                </button>
-              </div>
 
-            </ul>
+<!-- Cracion de navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://i.ibb.co/KKnBn4P/1-removebg-preview.png" width="40" height="40" class="d-inline-block align-mid"
+        alt="">
+      DEBEDE Music
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav right-left">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Quienes somos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ayuda</a>
+        </li>
+
+        <body style="background-color:rgb(33, 37, 41);">
+          <div class="d-flex align-items-right">
+            <button type="button" class="btn btn-link pl-auto me-2">
+              Iniciar sesión
+            </button>
+            <button type="button" class="btn btn-primary pl-auto me-3">
+              Registrarse
+            </button>
           </div>
-        </div>
-
-      </nav>
-      <div id="grid1">
-        <!-- Carousel images of albums  data-bs-ride="carousel"-->
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-               <h1>
-                 Text 1
-               </h1>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, consequatur ratione. Ullam eaque rerum mollitia iure, aliquid quia molestias rem atque magni maiores architecto dignissimos voluptas esse, porro praesentium ipsam!</p>
-              </div>
-              <div class="carousel-item">
-                <h1>
-                  Text 2
-                </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, consequatur ratione. Ullam eaque rerum mollitia iure, aliquid quia molestias rem atque magni maiores architecto dignissimos voluptas esse, porro praesentium ipsam!</p>
-              
-              </div>
-              <div class="carousel-item">
-                <h1>
-                  Text 3
-                </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, consequatur ratione. Ullam eaque rerum mollitia iure, aliquid quia molestias rem atque magni maiores architecto dignissimos voluptas esse, porro praesentium ipsam!</p>
-              
-              </div>
-            </div>
-          </div>
-          <!--Termino Div-->
-       </div>
 
 
+      </ul>
+    </div>
+  </div>
 
+</nav>
+<div id="grid1">
+  <div class="slideshow-container">
 
+    <div class="mySlides fade">
+      <div class="numbertext">1 / 3</div>
+      <!--Aqui van las images-->
+      <h1> Por que DEBEDE?</h1>
 
-       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </div>
+
+    <div class="mySlides fade">
+      <div class="numbertext">2 / 3</div>
+
+      <!--Aqui van las images-->
+      <h1> Descubre música</h1>
+
+    </div>
+
+    <div class="mySlides fade">
+      <div class="numbertext">3 / 3</div>
+
+      <!--Aqui van las images <img src="img_nature_wide.jpg" style="width:50%">-->
+      <h1>Alucina con nuestros precios</h1>
+
+    </div>
+
+  </div>
+  <br>
+
+  <!---->
+  <div style="text-align:center">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+  </div>
+
+  <script>
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) { slideIndex = 1 }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+  </script>
+  <!--Termino Div-->
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
+<footer style="color:aliceblue">
+  DEBEDE todos los izquierdos reservados.
+</footer>
 
 </html>
